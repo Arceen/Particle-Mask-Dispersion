ca4b73178550b499e41780eb31b1016ad5e9f4e4
Arceen <niloyman01@gmail.com>
2022-12-15 23:41:26 +0600

    1 file modified

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const random = require('canvas-sketch-util/random');
 const math = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [1080, 1080],
+  dimensions: [2084, 2084],
   animate: true,
 };
+let elCanvas;
+const cursor = { x: 10000, y: 10000 };
 const particles = [];
-const particleCount = 10;
-const sketch = ({ context, width, height }) => {
+const particleCount = 2;
+const sketch = ({ context, width, height, canvas }) => {
+  elCanvas = canvas;
+  elCanvas.addEventListener('mousedown', onMouseDown);
   for (let i = 0; i < particleCount; i++) {
     particles.push(
       new Particle({
         x: random.range(0, width),
         y: random.range(0, height),
+        // x: height * 0.5,
+        // y: height * 0.5,
         radius: random.range(10, 20),
       })
     );
@@ -44,11 +50,33 @@ class Particle {
     this.iy = y;
 
     this.radius = radius;
+    this.minDist = 100;
+    this.pushFactor = 0.02;
+    this.pullFactor = 0.004;
+    this.dampingFactor = 0.95;
   }
   update() {
+    let dx, dy, dd, distanceDelta;
+
+    dx = this.ix - this.x;
+    dy = this.iy - this.y;
+    this.ax = dx * this.pullFactor;
+    this.ay = dy * this.pullFactor;
+
+    dx = this.x - cursor.x;
+    dy = this.y - cursor.y;
+    dd = Math.sqrt(dx * dx - dy * dy);
+    distanceDelta = this.minDist - dd;
+    if (dd < this.minDist) {
+      this.ax = (dx / dd) * distanceDelta * this.pushFactor;
+      this.ay = (dy / dd) * distanceDelta * this.pushFactor;
+    }
+
     this.ax += 0.001;
     this.vx += this.ax;
     this.vy += this.ay;
+    this.vx *= this.dampingFactor;
+    this.vy *= this.dampingFactor;
     this.x += this.vx;
     this.y += this.vy;
   }
@@ -61,9 +89,27 @@ class Particle {
     context.arc(0, 0, this.radius, 0, 2 * Math.PI);
     // context.stroke();
     context.fill();
-
     context.restore();
   }
 }
+
+const onMouseDown = (e) => {
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+  onMouseMove(e);
+};
+
+const onMouseMove = (e) => {
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+  cursor.x = x;
+  cursor.y = y;
+};
+const onMouseUp = (e) => {
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
+  cursor.x = 10000;
+  cursor.y = 10000;
+};
 
 canvasSketch(sketch, settings);
